f78683e40fb6a2d14a14fc53382f7d5a1088789c
Eimaytto <louis04060@gmail.com>
2019-05-22 22:05:36 +0800

    Update script_V2.js

--- js/script_V2.js
+++ js/script_V2.js
@@ -7,51 +7,51 @@ function output(){
 			answer = "도울 좌,평평할 평";
 			break;
 		case 2:
-			message.innerHTML = "等级";
+			document.getElementById('message').innerHTML = "等级";
 			answer = "등급 등,등급 급";
 			break;
 		case 3:
-			message.innerHTML = "倂合";
+			document.getElementById('message').innerHTML = "倂合";
 			answer = "아우를 병,합할 합";
 			break;
 		case 4:
-			message.innerHTML = "戰死";
+			document.getElementById('message').innerHTML = "戰死";
 			answer = "싸울 전,죽을 사";
 			break;
 		case 5:
-			message.innerHTML = "地域";
+			document.getElementById('message').innerHTML = "地域";
 			answer = "땅 지,지경 역";
 			break;
 		case 6:
-			message.innerHTML = "王族";
+			document.getElementById('message') = "王族";
 			answer = "임금 왕,겨레 족";
 			break;
 		case 7:
-			message.innerHTML = "國交";
+			document.getElementById('message') = "國交";
 			answer = "나라 국,사귈 교";
 			break;
 		case 8:
-			message.innerHTML = "王號";
+			document.getElementById('message') = "王號";
 			answer = "임금 왕,부를 호";
 			break;
 		case 9:
-			message.innerHTML = "國號";
+			document.getElementById('message') = "國號";
 			answer = "나라 국,부를 호";
 			break;
 		case 10:
-			message.innerHTML = "農事";
+			document.getElementById('message') = "農事";
 			answer = "농사 농,일 사";
 			break;
 		case 11:
-			message.innerHTML = "上大等";
+			document.getElementById('message') = "上大等";
 			answer = "위 상,큰 대,등급 등";
 			break;
 		case 12:
-			message.innerHTML = "兵部";
+			document.getElementById('message') = "兵部";
 			answer = "병사 병,떼 부";
 			break;
 		case 13:
-			message.innerHTML = "花郞徒";
+			document.getElementById('message') = "花郞徒";
 			answer = "꽃 화,사내 랑,무리 도";
 			break;
 	}
